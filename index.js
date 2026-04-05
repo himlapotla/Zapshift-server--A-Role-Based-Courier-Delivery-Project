e929f0c7c707567b67401db262120b5ee31ecef0
potla <shahriarhimel00@gmail.com>
2026-04-06 01:09:36 +0600

    server setup is ongoing

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Zap is Shifting..!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
